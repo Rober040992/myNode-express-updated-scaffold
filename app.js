5e18d219259ca6739d975c3669029e3ac9c21334
RoberGF <rgfrasta@gmail.com>
2025-06-20 20:08:32 +0200

    router added

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
-import { join } from 'node:path'
-import express from 'express'
-import createError from 'http-errors'
-import logger from 'morgan'
-import connectMongoose from './lib/mongooseConfig.js'
+import { join } from "node:path";
+import express from "express";
+import createError from "http-errors";
+import logger from "morgan";
+import connectMongoose from "./lib/mongooseConfig.js";
+import routes from "./routes/Routes.js";
 //API controllers imports
 
-// Web Site controller 
+await connectMongoose();
+console.log("✅ MongoDB connected!");
 
-const app = express()
-
-// view engine setup
-app.set('views', join(import.meta.dirname, 'views'))
-app.set('view engine', 'ejs')
-
-// middlewares
+const app = express();
 
 // morgan logger for http requests logs
-app.use(logger('dev'))
+app.use(logger("dev"));
 // transforms json objects into js objects
-app.use(express.json())
+app.use(express.json());
 // transforms data sent by a form to a js object
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 // set the folder where statis resources will be served
-app.use(express.static(join(import.meta.dirname, 'public')))
+app.use(express.static(join(import.meta.dirname, "public")));
+// show where static images are and where to serve them from
+app.use('/images', express.static(join(import.meta.dirname, 'public/images')));
 
 // Routing
-// index route rendering home.ejs
-app.get('/', (req, res) => {
-  res.render('home')
-})
+app.use("/", routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+  res.status(err.status || 500);
+  res.render("error");
+});
 
-export default app
+export default app;
